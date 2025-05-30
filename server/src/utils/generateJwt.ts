43be01b96b3229dc87f5d26839aded8067f80403
aomist <sudippaudel512@gmail.com>
2025-05-30 06:06:02 +0545

    jwt generation and cookies

---
+++ server/src/utils/generateJwt.ts
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+import { Response } from "express";
+const ST = process.env.SECRET_KEY;
+export const generateJwtAndSetCookie = async (
+  userId: string,
+  res: Response
+) => {
+  const token = jwt.sign({ userId }, ST as string, {
+    expiresIn: "15d",
+  });
+  res.cookie("token", token, {
+    maxAge: 15 * 24 * 3600 * 1000,
+  });
+};
