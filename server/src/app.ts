4da45d5bd122b41a306fc8e27e0bed1befab584b
Bladex <sudippaudel512@gmail.com>
2025-08-26 23:45:57 +0545

    added Converstaion route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import userRouter from "./controllers/user.controller";
+import postRouter from "./controllers/post .controller";
+import chatRouter from "./controllers/conversation.controller";
+import middleware from "./middlewares/middleware";
 const app = express();
 
 app.use(express.json());
@@ -10,11 +14,13 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow cookies or credentials to be sent
-  })
+  }),
 );
 app.use(cookieParser());
-import userRouter from "./routes/user.route";
-import postRouter from "./routes/post.route";
+
+app.use(middleware.requestLogger);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/chats", chatRouter);
+
 export default app;
